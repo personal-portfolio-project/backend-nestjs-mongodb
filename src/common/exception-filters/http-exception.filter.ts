975b238d0ebce76671e44f008fe2b5762e6a573a
Abdul Raziq Advani <araziq96@hotmail.com>
2022-02-03 16:54:14 +0500

    Refactor the code

--- src/common/exception-filters/http-exception.filter.ts
+++ src/common/exception-filters/http-exception.filter.ts
@@ -4,13 +4,12 @@ import {
   ArgumentsHost,
   HttpException,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    // const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const errorMessage = exception.getResponse();
